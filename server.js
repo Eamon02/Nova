fb3cd0f18d16b08dda6e9a3ad58817782763c00d
Eamon McHugh <eamonmchugh2@gmail.com>
2021-04-09 15:16:25 -0400

    deleted client, pulling drew client

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const connectDB = require('./database/db');
 const app = express();
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8080;
 
 // Connect Database
 connectDB();

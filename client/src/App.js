83702c8f992a075d2c1bf61ee9d501ddf17afd3a
Eamon McHugh <eamonmchugh2@gmail.com>
2021-04-12 13:16:41 -0400

    finished tutorial

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,45 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import { Navbar } from './components/layout/Navbar';
-import { Register } from './components/auth/Register';
-import { Login } from './components/auth/Login';
+import Routes from './components/routing/Routes';
+import { LOGOUT } from './actions/types';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
 import './App.css';
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    // check for token in LS
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+    store.dispatch(loadUser());
+
+    // log user out from all tabs if they log out in one tab
+    window.addEventListener('storage', () => {
+      if (!localStorage.token) store.dispatch({ type: LOGOUT });
+    });
+  }, []);
+
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <section className="container">
-            <Switch>
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-            {/* <Route component={Routes} /> */}
+    <Provider store={store}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route component={Routes} />
           </Switch>
-        </section>
-      </Fragment>
-    </Router>
-    
+        </>
+      </Router>
+    </Provider>
   );
-}
+};
 
 export default App;
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Link,  } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default function Landing() {
+const Landing = ({ isAuthenticated }) => {
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard' />;
+  }
+
   return (
-    <section class='landing'>
-      <div class='dark-overlay'>
-        <div class='landing-inner'>
-          <h1 class='x-large'>Developer Connector</h1>
-          <p class='lead'>
-            Create a developer profile/portfolio, share posts and get help from
-            other developers this is fanstastic
-          </p>
-          <div class='buttons'>
-           <Link to='/register' className='btn btn-primary'>
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Nova</h1>
+          <div className='buttons'>
+            <Link to='/register' className='btn btn-primary'>
               Sign Up
             </Link>
             <Link to='/login' className='btn btn-light'>
@@ -23,4 +25,14 @@ export default function Landing() {
       </div>
     </section>
   );
-}
+};
+
+Landing.propTypes = {
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps)(Landing);

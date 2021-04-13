c621b35e50c19be6104858ef78c84596159268eb
Eamon McHugh <eamonmchugh2@gmail.com>
2021-04-13 18:13:11 -0400

    mainpage route on login

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -13,6 +13,7 @@ import Posts from '../posts/Posts';
 import Post from '../post/Post';
 import NotFound from '../layout/NotFound';
 import PrivateRoute from '../routing/PrivateRoute';
+import MainPage from '../MainPage/MainPage';
 
 const Routes = props => {
   return (
@@ -23,6 +24,7 @@ const Routes = props => {
         <Route exact path="/login" component={Login} />
         <Route exact path="/profiles" component={Profiles} />
         <Route exact path="/profile/:id" component={Profile} />
+        <PrivateRoute exact path="/mainpage" component={MainPage} />
         <PrivateRoute exact path="/dashboard" component={Dashboard} />
         <PrivateRoute exact path="/create-profile" component={ProfileForm} />
         <PrivateRoute exact path="/edit-profile" component={ProfileForm} />

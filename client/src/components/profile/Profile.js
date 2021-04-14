b8a404d08aa317e3217e7f18bd216a37cd8d21fb
Eamon McHugh <eamonmchugh2@gmail.com>
2021-04-14 19:02:11 -0400

    error from components/profile/profile.js

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,7 +6,7 @@ import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
-// import ProfileEducation from './ProfileEducation';
+import ProfileEducation from './ProfileEducation';
 import ProfileRigs from './ProfileRigs';
 import ProfileGithub from './ProfileGithub';
 import { getProfileById } from '../../actions/profile';
@@ -22,21 +22,21 @@ const Profile = ({ getProfileById, profile: { profile }, auth, match }) => {
         <Spinner />
       ) : (
         <>
-          <Link to="/profiles" className="btn btn-light">
+          <Link to='/profiles' className='btn btn-light'>
             Back To Profiles
           </Link>
           {auth.isAuthenticated &&
             auth.loading === false &&
             auth.user._id === profile.user._id && (
-              <Link to="/edit-profile" className="btn btn-dark">
+              <Link to='/edit-profile' className='btn btn-dark'>
                 Edit Profile
               </Link>
             )}
-          <div className="profile-grid my-1">
+          <div className='profile-grid my-1'>
             <ProfileTop profile={profile} />
             <ProfileAbout profile={profile} />
-            <div className="profile-exp bg-white p-2">
-              <h2 className="text-primary">Experience</h2>
+            <div className='profile-exp bg-white p-2'>
+              <h2 className='text-primary'>Experience</h2>
               {profile.experience.length > 0 ? (
                 <>
                   {profile.experience.map((experience) => (
@@ -51,8 +51,8 @@ const Profile = ({ getProfileById, profile: { profile }, auth, match }) => {
               )}
             </div>
 
-            {/* <div className="profile-edu bg-white p-2">
-              <h2 className="text-primary">Education</h2>
+            <div className='profile-edu bg-white p-2'>
+              <h2 className='text-primary'>Education</h2>
               {profile.education.length > 0 ? (
                 <>
                   {profile.education.map((education) => (
@@ -65,23 +65,20 @@ const Profile = ({ getProfileById, profile: { profile }, auth, match }) => {
               ) : (
                 <h4>No education credentials</h4>
               )}
-            </div> */}
-            
-          <div className="profile-edu bg-white p-2">
-              <h2 className="text-primary">Education</h2>
-              {profile.education.length > 0 ? (
+            </div>
+{/* 
+            <div className='profile-edu bg-white p-2'>
+              <h2 className='text-primary'>Rigs</h2>
+              {profile.rigs.length > 0 ? (
                 <>
-                  {profile.education.map((education) => (
-                    <ProfileRigs
-                      key={education._id}
-                      education={education}
-                    />
+                  {profile.rigs.map((rigs) => (
+                    <ProfileRigs key={rigs._id} education={rigs} />
                   ))}
                 </>
               ) : (
                 <h4>No education credentials</h4>
               )}
-            </div>
+            </div> */}
 
             {profile.githubusername && (
               <ProfileGithub username={profile.githubusername} />
@@ -96,12 +93,12 @@ const Profile = ({ getProfileById, profile: { profile }, auth, match }) => {
 Profile.propTypes = {
   getProfileById: PropTypes.func.isRequired,
   profile: PropTypes.object.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   profile: state.profile,
-  auth: state.auth
+  auth: state.auth,
 });
 
 export default connect(mapStateToProps, { getProfileById })(Profile);
